8343763f0bf1663ae147492c7e7a4d2a906b85ab
Ysk-him1 <2425403809@live.gctu.edu.gh>
2026-05-31 01:52:54 +0000

    assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,30 @@
+# Name: Prince Emmanuel Ashong
+# Student ID: 2425403809
+# Email: 2425403809@live.gctu.edu.gh
+# Assignment: Initials
+
 
 def main():
-  pass #code goes here
+    # Prints stylized initials
 
+    print()
+
+    print("...PEA")
+
+    print()
+
+    print("...PPPPPPPPPPPP***...EEEEEEEEEEEE***...AAAAAAAAAAAA***")
+    print("...PP........PP***...EE..........***...AA........AA***")
+    print("...PP........PP***...EE..........***...AA........AA***")
+    print("...PP........PP***...EE..........***...AA........AA***")
+    print("...PPPPPPPPPPPP***...EEEEEEEEEEEE***...AAAAAAAAAAAA***")
+    print("...PP..........***...EE..........***...AA........AA***")
+    print("...PP..........***...EE..........***...AA........AA***")
+    print("...PP..........***...EE..........***...AA........AA***")
+    print("...PP..........***...EE..........***...AA........AA***")
+    print("...PP..........***...EEEEEEEEEEEE***...AA........AA")
+
+    print()
 
 
 main()
